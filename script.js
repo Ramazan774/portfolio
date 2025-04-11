9890811cb6e25465d29a469a3201e5dbe5abbe81
Ramazan774 <ramazan.ramazanov@glion.ch>
2025-04-10 21:03:32 -0500

    working first draft ready

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
+const linksInNav = document.querySelectorAll('.nav-links a');
 
 menuIcon.onclick = () => {
   navLinks.classList.toggle('active');
+  if (navLinks.classList.contains('active')){
+    menuIcon.classList.remove('fa-bars');
+    menuIcon.classList.add('fa-times');
+  } else {
+    menuIcon.classList.remove('fa-times');
+    menuIcon.classList.add('fa-bars');
+  }
 }
+
+linksInNav.forEach((link) => {
+  link.onclick = () => {
+    navLinks.classList.remove('active');
+    menuIcon.classList.remove('fa-times');
+    menuIcon.classList.add('fa-bars');
+  }
+})
